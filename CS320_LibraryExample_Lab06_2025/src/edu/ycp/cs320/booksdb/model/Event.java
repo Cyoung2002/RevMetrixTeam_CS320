a3a6f2647898e55c4806fa2339e3e94be3b7593c
Caroline Young <Cyoung29@ycp.edu>
2025-04-26 14:55:27 -0400

    Working At the moment

---
+++ CS320_LibraryExample_Lab06_2025/src/edu/ycp/cs320/booksdb/model/Event.java
@@ -0,0 +1,82 @@
+package edu.ycp.cs320.booksdb.model;
+
+public class Event {
+	//private int establishmentId;
+	private String longname;
+	private String shortname;
+	private String establishmentShort;
+	private String weeknight;
+	private String start;
+	private String end;
+	private int gamesPerSession;
+	
+	
+	public Event() {
+		
+	}
+
+	/*
+	public int getEstablishmentId() {
+		return establishmentId;
+	}
+	public void setEstablishmentId(int establishmentId) {
+		this.establishmentId = establishmentId;
+	}
+	*/
+
+	public String getLongname() {
+		return longname;
+	}
+	public void setLongname(String longname) {
+		this.longname = longname;
+	}
+
+
+	public String getShortname() {
+		return shortname;
+	}
+	public void setShortname(String shortname) {
+		this.shortname = shortname;
+	}
+	
+	
+	public String getEstablishmentShort() {
+		return establishmentShort;
+	}
+	public void setEstablishmentShort(String estabShort) {
+		this.establishmentShort = estabShort;
+	}
+
+
+	public String getWeeknight() {
+		return weeknight;
+	}
+	public void setWeeknight(String weeknight) {
+		this.weeknight = weeknight;
+	}
+
+
+	public String getStart() {
+		return start;
+	}
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+
+	public int getGamesPerSession() {
+		return gamesPerSession;
+	}
+	public void setGamesPerSession(int gamesPerSession) {
+		this.gamesPerSession = gamesPerSession;
+	}
+
+}
