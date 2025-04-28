1663a551e49d09398899a13bb679230a75068e8d
Tanner Sweigart <tsweigart@ycp.edu>
2025-04-27 22:49:03 -0400

    Insert event and find all event test working!

--- CS320_LibraryExample_Lab06_2025/junit/edu/ycp/cs320/booksdb/persist/DerbyDatabaseTests.java
+++ CS320_LibraryExample_Lab06_2025/junit/edu/ycp/cs320/booksdb/persist/DerbyDatabaseTests.java
@@ -15,6 +15,7 @@ import edu.ycp.cs320.booksdb.model.Author;
 import edu.ycp.cs320.booksdb.model.Ball;
 import edu.ycp.cs320.booksdb.model.Book;
 import edu.ycp.cs320.booksdb.model.Establishment;
+import edu.ycp.cs320.booksdb.model.Event;
 import edu.ycp.cs320.booksdb.model.Pair;
 
 public class DerbyDatabaseTests {
@@ -28,6 +29,7 @@ public class DerbyDatabaseTests {
 	
 	ArrayList<Ball> arsenal = null;
 	ArrayList<Establishment> establishments = null;
+	ArrayList<Event> event = null;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -235,7 +237,7 @@ public class DerbyDatabaseTests {
 		}
 	}
 	
-	//test was created on 4/27/25 @ 10pm, this test shows a new establishment being added
+	//test was created on 4/27/25 @ 10pm, this test shows a new ball being added to arsenal
 	@Test
 	public void testInsertBallIntoArsenal() {
 		System.out.println("\n*** Testing insertBallIntoArsenal ***");
@@ -282,6 +284,50 @@ public class DerbyDatabaseTests {
 		{
 			System.out.println("Failed to insert new ball: " + longname + ") into arsenal with ID: <" + ball_id + ">");
 			fail("Failed to insert new ball <" + longname + "> into DB");
+		}
+	}
+	
+	//test was created on 4/27/25 @ 10:30pm, this test shows a new event being added to the event list
+	@Test
+	public void testInsertEvent() {
+		System.out.println("\n*** Testing insertEvent ***");
+
+		String longname 			= "Summer Women's Duo";
+		String shortname 			= "SWD"; 
+		String establishmentShort 	= "SB"; 
+		String weeknight 			= "33";
+		String start 				= "05/15/25"; 
+		String end_date 			= "08/20/25";
+		Integer gamesPerSession 	= 3;
+					
+		// insert new event (and possibly new author) into DB
+		Integer event_id = db.insertEvent(longname, shortname, establishmentShort, weeknight, start, end_date, gamesPerSession);
+
+		// check the return value - should be a book_id > 0
+		if (event_id > 0)
+		{
+			// try to retrieve the book and author from the DB
+			// get the list of (Author, Book) pairs from DB
+			event = db.findAllEvents();
+		
+			if (establishments.isEmpty()) {
+				System.out.println("No events found within event table <" + event_id + ">");
+				fail("Failed to insert new event <" + longname + "> into DB");
+			}
+			// otherwise, the test was successful.  Now remove the book just inserted to return the DB
+			// to it's original state, except for using an author_id and a book_id
+			else {
+				System.out.println("New event: " + longname + ") successfully added to event table: <" + event_id + ">");
+					
+				// now delete establishment from DB
+				// leaving the DB in its previous state - except that an author_id, and a book_id have been used
+				//we do not have this function implemented yet
+			}
+		}
+		else
+		{
+			System.out.println("Failed to insert new event: " + longname + ") into event table: <" + event_id + ">");
+			fail("Failed to insert new event <" + longname + "> into DB");
 		}
 	}
 	
