221a194ced4b92995d2e52c4e7e453fead53db64
creppert308 <creppert@ycp.edu>
2025-05-02 13:56:54 -0400

    Updated Event Class

--- CS320_LibraryExample_Lab06_2025/src/edu/ycp/cs320/booksdb/model/Event.java
+++ CS320_LibraryExample_Lab06_2025/src/edu/ycp/cs320/booksdb/model/Event.java
@@ -4,58 +4,95 @@ public class Event {
 	//private int establishmentId;
 	private String longname;
 	private String shortname;
-	private String establishmentShort;
-	private String weeknight;
+	private String type;
+	private String establishment;
+	private String season;
+	private int team;
+	private String composition;
+	private String day;
+	private String time;
 	private String start;
 	private String end;
 	private int gamesPerSession;
-	private String eventID;
+	private int weeks;
+	private int playoffs;
 	
 	
 	public Event() {
 		
 	}
 
-	/*
-	public int getEstablishmentId() {
-		return establishmentId;
-	}
-	public void setEstablishmentId(int establishmentId) {
-		this.establishmentId = establishmentId;
-	}
-	*/
-
 	public String getLongname() {
 		return longname;
 	}
+	
 	public void setLongname(String longname) {
 		this.longname = longname;
 	}
 
-
 	public String getShortname() {
 		return shortname;
 	}
+	
 	public void setShortname(String shortname) {
 		this.shortname = shortname;
 	}
 	
+	public String getType() {
+		return type;
+	}
 	
-	public String getEstablishmentShort() {
-		return establishmentShort;
+	public void setType(String type) {
+		this.type = type;	
 	}
-	public void setEstablishmentShort(String estabShort) {
-		this.establishmentShort = estabShort;
+	
+	public String getEstablishment() {
+		return establishment;
+	}
+	
+	public void setEstablishment(String establishment) {
+		this.establishment = establishment;
+	}
+	
+	public String getSeason() {
+		return season;
+	}
+	
+	public void setSeason(String season) {
+		this.season = season;
+	}
+	
+	public int getTeam() {
+		return team;
+	}
+	
+	public void setTeam(int team) {
+		this.team = team;
+	}
+	
+	public String getComposition() {
+		return composition;
+	}
+	
+	public void setComposition(String composition) {
+		this.composition = composition;
 	}
 
-
-	public String getWeeknight() {
-		return weeknight;
+	public String getDay() {
+		return day;
 	}
-	public void setWeeknight(String weeknight) {
-		this.weeknight = weeknight;
+	
+	public void setDay(String day) {
+		this.day = day;
 	}
-
+	
+	public String getTime() {
+		return time;
+	}
+	
+	public void setTime(String time) {
+		this.time = time;
+	}
 
 	public String getStart() {
 		return start;
@@ -64,14 +101,12 @@ public class Event {
 		this.start = start;
 	}
 
-
 	public String getEnd() {
 		return end;
 	}
 	public void setEnd(String end) {
 		this.end = end;
 	}
-
 
 	public int getGamesPerSession() {
 		return gamesPerSession;
@@ -80,8 +115,19 @@ public class Event {
 		this.gamesPerSession = gamesPerSession;
 	}
 	
-	public void setEventID(String eventID) {
-		this.eventID = eventID;
+	public int getWeeks() {
+		return weeks;
 	}
-
+	
+	public void setWeeks(int weeks) {
+		this.weeks = weeks;
+	}
+	
+	public int getPlayoffs() {
+		return playoffs;
+	}
+	
+	public void setPlayoffs(int playoffs) {
+		this.playoffs = playoffs;
+	}
 }
