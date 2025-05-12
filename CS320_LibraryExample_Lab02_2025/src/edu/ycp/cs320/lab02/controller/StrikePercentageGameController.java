30bf8122196042d7479f286db8b7775f619f70a5
creppert308 <creppert@ycp.edu>
2025-05-11 22:23:54 -0400

    Update StrikePercentageGameController.java

--- CS320_LibraryExample_Lab02_2025/src/edu/ycp/cs320/lab02/controller/StrikePercentageGameController.java
+++ CS320_LibraryExample_Lab02_2025/src/edu/ycp/cs320/lab02/controller/StrikePercentageGameController.java
@@ -16,44 +16,6 @@ public class StrikePercentageGameController {
 		DatabaseProvider.setInstance(new DerbyDatabase());
 		db = DatabaseProvider.getInstance();		
 	}
-
-	public Double AllStrikePercentage() {
-		
-		// insert new book (and possibly new author) into DB
-		ArrayList<Shot> shotList = db.findAllShots();
-		Double numStrikes = 0.0;
-		Double numFirstShots = 10.0;
-		Double percentResult = 0.0;
-		
-		for (Shot shot : shotList) {
-			if (shot.getCount().equals("X")) {
-			    numStrikes++;
-			}
-		}
-		
-		System.out.println(numStrikes);
-		percentResult = (((numStrikes)/(numFirstShots))*100.0);
-		return percentResult;
-	}
-	
-	public Double StrikePercentageFrame(String frameNum) {
-		
-		// insert new book (and possibly new author) into DB
-		ArrayList<Shot> shotList = db.findAllShotsInFrame(frameNum);
-		Double numStrikes = 0.0;
-		Double numFirstShots = 10.0;
-		Double percentResult = 0.0;
-		
-		for (Shot shot : shotList) {
-			if (shot.getCount().equals("X")) {
-			    numStrikes++;
-			}
-		}
-		
-		System.out.println(numStrikes);
-		percentResult = (((numStrikes)/(numFirstShots))*100.0);
-		return percentResult;
-	}
 	
 	public Double StrikePercentageEventFrame(String event, String season, String frameNum) {
 		ArrayList<Shot> shotList = db.findAllShotsGivenEventFrame(event, season, frameNum);
